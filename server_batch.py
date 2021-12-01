f4ec2c705e45de378731766b680010ce29f8ec29
Batyrlan Nurbekov <batyrlan94@gmail.com>
2021-11-30 18:12:57 -0800

    Adding comments.

--- server_batch.py
+++ server_batch.py
@@ -4,39 +4,46 @@ import asyncio
 
 MAX_TOKENS = 4778 # HACK: determined the maximum amount of tokens for Nvidia T4 (16 Gb VRAM) 
                   # by manually tuning the number of items in a batch with items of sequence length of 1024
+MAX_QUEUE_SIZE = 20
+DEFAULT_MAX_SEQ_LEN = 20
 
 inference_model = InferenceModel()
 
 app = FastAPI()
 
-q = asyncio.Queue(20)
+q = asyncio.Queue(MAX_QUEUE_SIZE)
+
 async def process_batches():
-    prev_item = None
+    first_item = None
     while True:
-        if prev_item is None:
-            prev_item = await q.get()
-        items = [prev_item]
-        batch_max_len = prev_item[0]['max_length']
-        while True: # dequeue until we get an exception which means that the queue is empty
+        if first_item is None:
+            first_item = await q.get() # wait for the next item (blocking)
+        items = [first_item]
+        batch_max_len = first_item[0]['max_length']
+        
+        # dequeue until we get an exception which means that the queue is empty
+        while True: 
             q_item = None
             try:
                 q_item = q.get_nowait()
             except Exception as e:
                 print("Exception while getting the future", e)
-                prev_item = None
+                first_item = None
                 break
             if q_item is not None:
                 curr_max_len = max(batch_max_len, q_item[0]['max_length']) 
-                print("curr_max_len * (len(items) + 1): ", curr_max_len * len(items))
                 if curr_max_len * len(items) < MAX_TOKENS:
                     items.append(q_item)
                     batch_max_len = curr_max_len
                 else:
-                    prev_item = q_item
+                    # Since adding this item to the batch will produce a batch that is too large to fit into a memory,
+                    # we use it as the first item during the next iteration.
+                    first_item = q_item
                     break
         
         outputs = inference_model.run_batch_inference([item[0] for item in items])
 
+        # resolving the futures (setting the results)
         for i, output in enumerate(outputs):
             items[i][1].set_result(output)
         # HACK: the following line ensures that the futures are resolved 
@@ -54,7 +61,7 @@ async def root(request: Request):
         raise HTTPException(status_code=400, detail="Prompt needs to provided as an input parameter")
     
     if "max_length" not in params_json:
-        params_json["max_length"] = 20
+        params_json["max_length"] = DEFAULT_MAX_SEQ_LEN
     
     fut = loop.create_future()
     try:
